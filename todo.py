636f05cbc7c973b6fdeb13e927b26551bee8fb5d
formateli <formateli@gmail.com>
2020-09-23 21:43:05 -0500

    Add done button on tree view

--- todo.py
+++ todo.py
@@ -8,31 +8,26 @@ from trytond.model import (
         fields, sequence_ordered, tree)
 from trytond.pyson import Eval, In, Equal, If, Bool, And
 from trytond.i18n import gettext
-from trytond.exceptions import UserError
+from trytond.exceptions import UserWarning
 import datetime
 from pytz import timezone
-
-__all__ = ['Todo']
-
-STATES = [
-    ('open', 'Open'),
-    ('done', 'Done'),
-    ]
-
-_STATES = {
-    'readonly': Equal(Eval('state'), 'done'),
-    }
 
 
 class Todo(Workflow, ModelSQL, ModelView,
            sequence_ordered(), tree(separator=' / ')):
-    'Todo'
+    'TODO task'
     __name__ = 'todo.todo'
+
+    _states = {
+        'readonly': Equal(Eval('state'), 'done'),
+        }
+    _depends = ['state']
+
     name = fields.Char('Name', required=True,
-        states=_STATES, depends=['state'])
+        states=_states, depends=_depends)
     date = fields.Function(fields.DateTime('Date'), 'get_date')
     limit_date = fields.DateTime('Limit Date',
-        states=_STATES, depends=['state'])
+        states=_states, depends=_depends)
     limit_state = fields.Function(
         fields.Integer('Limit state'),
         'get_limit_state')
@@ -40,20 +35,25 @@ class Todo(Workflow, ModelSQL, ModelView,
         states={
             'readonly': Equal(Eval('state'), 'done'),
             'invisible': Equal(Eval('state'), 'open')
-            }, depends=['state'])
+            }, depends=_depends)
     user = fields.Function(fields.Many2One('res.user', 'User'), 'get_user')
     parent = fields.Many2One('todo.todo', 'Parent', select=True,
         domain=[
             ('create_uid', '=', Eval('create_uid'))
         ],
-        states=_STATES, depends=['state', 'create_uid'])
+        states=_states, depends=_depends + ['create_uid'])
     childs = fields.One2Many('todo.todo', 'parent', string='Childs',
-        states=_STATES, depends=['state'])
+        states=_states, depends=_depends)
     childs_open = fields.One2Many('todo.todo', 'parent', string='Childs Open',
-        states=_STATES, depends=['state'], filter=[('state', '=', 'open')])
+        states=_states, depends=_depends, filter=[('state', '=', 'open')])
     description = fields.Text('Description',
-        states=_STATES, depends=['state'])
-    state = fields.Selection(STATES, 'State', readonly=True, required=True)
+        states=_states, depends=_depends)
+    state = fields.Selection([
+        ('open', 'Open'),
+        ('done', 'Done'),
+        ], 'State', readonly=True, required=True)
+
+    del _states, _depends
 
     @classmethod
     def __setup__(cls):
@@ -128,14 +128,6 @@ class Todo(Workflow, ModelSQL, ModelView,
         return [('create_date',) + tuple(clause[1:])]
 
     @classmethod
-    def validate(cls, todos):
-        for todo in todos:
-            if todo.limit_date and todo.limit_date < todo.date:
-                raise UserError(
-                    gettext('todo.msg_invalid_limit_date',
-                        name=todo.rec_name))
-
-    @classmethod
     @ModelView.button
     @Workflow.transition('open')
     def open(cls, todos):
@@ -145,6 +137,28 @@ class Todo(Workflow, ModelSQL, ModelView,
     @ModelView.button
     @Workflow.transition('done')
     def done(cls, todos):
+        finish_date = datetime.datetime.now()
+        to_done = cls._set_done(todos, finish_date, False)
+        cls.save(to_done)
+
+    @classmethod
+    def _set_done(cls, todos, finish_date, done_childs):
+        pool = Pool()
+        Warning = pool.get('res.user.warning')
+        to_done = []
         for todo in todos:
-            todo.finish_date = datetime.datetime.now()
-        cls.save(todos)
+            todo.state = 'done'
+            todo.finish_date = finish_date
+            to_done.append(todo)
+            if todo.childs:
+                if not done_childs:
+                    msg_id = 'todo_done_childs'
+                    if Warning.check(msg_id):
+                        raise UserWarning(
+                            msg_id,
+                            gettext(
+                                'todo.msg_todo_done_childs',
+                                todo=todo.rec_name)
+                            )
+                to_done += cls._set_done(todo.childs, finish_date, True)
+        return to_done
